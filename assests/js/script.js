cfccf851198ba95d52ece84d44f1bd63fde1de74
sabatohura <sabato@hura.one>
2023-07-19 22:45:20 +0200

    .

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,4 @@
+// Dom objectes
 const colorChoice = document.getElementById('colorChoice');
 const colorMode   = document.getElementById('colorMode');
 const EraserMode   = document.getElementById('EraserMode');
@@ -7,8 +8,7 @@ const sizeRange   = document.getElementById('sizeRange');
 const sizeSelector   = document.getElementById('sizeSelector');
 const gridField   = document.getElementById('gridField');
 
-
-
+//defaults
 const default_color = '#000';
 const default_mode  = 'color';
 const default_grid_size = 10;
@@ -23,10 +23,10 @@ const setCurrentGridSize = newGridSize => current_grid=newGridSize;
 
 const resetSketch = ()=>{
     flushGrid();
-    // setupGrid(default_grid_size);
+    setupGrid(default_grid_size);
 }
+const flushGrid=()=>gridField.innerHTML='';
 
-const flushGrid=()=>{gridField.innerHTML=''}
 
 colorChoice.oninput = e => setCurrentColor(e.target.value);
 colorMode.addEventListener('click', ()=> setCurrentMode('color'));
@@ -40,5 +40,45 @@ const changeSize =value=>{
     updateSizeValue(value)
     resetSketch();
 }
-
 const updateSizeValue=value=>sizeRange.innerHTML = value;
+
+const setupGrid=(size)=>{
+    gridField.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridField.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  
+    for (let i = 0; i < size * size; i++) {
+      const gridElement = document.createElement('div')
+      gridElement.classList.add('grid-element')
+      gridElement.addEventListener('mouseover', changeColor)
+      gridElement.addEventListener('mousedown', changeColor)
+      gridField.appendChild(gridElement)
+    }
+  }
+
+  function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (current_mode === 'color') {
+      e.target.style.backgroundColor = current_color
+    } else if (current_mode === 'erase') {
+      e.target.style.backgroundColor = '#fefefe'
+    }
+  }
+
+  function activateButton(newMode) {
+    if (current_mode == 'color') {
+        colorMode.classList.add('active')
+    } else if (current_mode == 'erase') {
+        EraserMode.classList.add('active')
+    }
+  
+    if (newMode === 'color') {
+        colorMode.classList.add('active')
+    } else if (newMode === 'erase') {
+        EraserMode.classList.add('active')
+    }
+  }
+  
+  window.onload = () => {
+    setupGrid(default_grid_size)
+    activateButton(default_color)
+  }
