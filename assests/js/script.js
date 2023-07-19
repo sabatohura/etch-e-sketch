33dd0a14b0f54601f5f8d22db73e7c09c73154a3
sabatohura <sabato@hura.one>
2023-07-19 17:40:30 +0200

    Set Js Variables

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,24 @@
+const colorChoice = document.getElementById('colorChoice');
+const colorMode   = document.getElementById('colorMode');
+const EraserMode   = document.getElementById('EraserMode');
+const reset   = document.getElementById('reset');
+const rangeDiv   = document.getElementById('rangeDiv');
+const sizeRange   = document.getElementById('sizeRange');
+const sizeSelector   = document.getElementById('sizeSelector');
+const gridField   = document.getElementById('gridField');
+
+
+
+const default_color = '#000';
+const default_mode  = 'color';
+const default_grid_size = 10;
+
+let current_color = default_color;
+let current_mode = default_mode;
+let current_grid = default_grid_size;
+
+const setCurrentColor = newColor => current_color=newColor;
+const setCurrentMode  = newMode => current_mode=newMode;
+const setCurrentGridSize = newGridSize => current_grid=newGridSize;
+
 
