93fbc49275e05fe5953b608d7841a8ffe86ab781
sabatohura <sabato@hura.one>
2023-07-25 02:23:26 +0200

    Fix issues with color

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,24 +1,47 @@
+// defaults 
 const default_grid_size = 10;
-const default_color = 'blue';
+const default_color = '#000000';
+
+let drawMode = false;
+// DOM objects
 const board = document.getElementById('board');
+const sizeRange = document.getElementById("sizeRange")
 
+// functions 
+const setColor    = value => newColor=value;
+const resetGrid   = () => window.onload();
+const setGridSize = value => newSize = value;
 
-const setColor = value => board.style.backgroundColor = value;
+const draw = () => {
+  sizeRange.innerHTML = newSize;
+  board.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`
+  board.style.gridTemplateRows = `repeat(${newSize}, 1fr)`
 
+  for (let i = 0; i < newSize * newSize; i++) {
+    const gridElement = document.createElement('div');
+    gridElement.classList.add('grid-element');
+    gridElement.addEventListener('mouseover', changeColor);
+    gridElement.addEventListener('mousedown', changeColor);
+    board.appendChild(gridElement)
+  }
+};
 
-
-function changeColor(e) {
-  e.target.style.backgroundColor = e
-  console.log(e)
-}
-
-function resetGrid()
-{
-  updateSizeValue(default_grid_size)
-  changeSize(default_grid_size)
-  sizeSelector.value(default_grid_size)
-}
+const setColorMode = () => {
+  drawMode = !drawMode;
+  if(drawMode)
+    {
+      document.getElementById('colorMode').innerHTML = 'Color Mode on'
+      draw()
+    }
+    else{
+      drawMode=false;
+    }
+};
 
 window.onload =()=>{
+  board.innerHTML = '';
   setColor(default_color);
+  setGridSize(default_grid_size);
 }
+
+const changeColor = e => e.target.style.backgroundColor = newColor;
