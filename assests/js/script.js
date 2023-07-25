0f3788058b98a2e78c8fb0e9f41ebdd30a63a062
sabatohura <sabato@hura.one>
2023-07-25 09:15:34 +0200

    Refactor with onchange Grid size to detect gridchange

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,8 @@
 // defaults 
 const default_grid_size = 10;
 const default_color = '#000000';
-
 let drawMode = false;
+
 // DOM objects
 const board = document.getElementById('board');
 const sizeRange = document.getElementById("sizeRange")
@@ -10,17 +10,19 @@ const sizeRange = document.getElementById("sizeRange")
 // functions 
 const setColor    = value => newColor=value;
 const resetGrid   = () => window.onload();
+const onGridSizeChanged =(value)=>{
+  resetGrid();
+  drawMode = false;
+  setGridSize(value);
+  setColorMode();
+}
 const setGridSize = value => {
   newSize = value;
-  sizeRange.innerHTML = newSize;
-
+  sizeRange.innerHTML = value;
 }
-
-
 const draw = () => {
   board.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`
   board.style.gridTemplateRows = `repeat(${newSize}, 1fr)`
-
   for (let i = 0; i < newSize * newSize; i++) {
     const gridElement = document.createElement('div');
     gridElement.classList.add('grid-element');
@@ -29,17 +31,21 @@ const draw = () => {
     board.appendChild(gridElement)
   }
 };
+const unDraw =() =>{
+  setColor('not a color');
+}
 const changeColor = e => e.target.style.backgroundColor = newColor;
 
 const setColorMode = () => {
   drawMode = !drawMode;
-  if(drawMode)
+  if(drawMode == true)
     {
-      document.getElementById('colorMode').innerHTML = 'Color Mode on'
-      draw()
+      document.getElementById('colorMode').innerHTML = 'Stop fill Color'
+      draw();
     }
     else{
-      drawMode=false;
+      document.getElementById('colorMode').innerHTML = 'Resume fill Color'
+      unDraw();
     }
 };
 
